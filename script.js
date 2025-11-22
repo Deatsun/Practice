0649a69cbb6287fc2e265e8bd35a65598875d145
Molnár Bence <mbbexonait@gmail.com>
2025-11-22 20:42:52 +0100

    LocalStorage practice

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-// Fuggvenyek
+//tomb
+let emberek = []; //localstorage miatt "let".
+
+//szerkesztes eldontese
+let szID = null;
+
+//DOM cache
+const nevInput = document.getElementById("nev");
+const korInput = document.getElementById("kor");
+const poziInput = document.getElementById("pozicio");
+const berInput = document.getElementById("ber");
+
+//Betöltés LocalStroage-ból
+const mentett = localStorage.getItem("emberek");
+if(mentett){
+    emberek = JSON.parse(mentett);
+    kiir();
+}
+
+//Fuggvenyek
+
+    //LocalStorage mentés
+    function mentes(){
+        localStorage.setItem("emberek", JSON.stringify(emberek));
+    }
+
     //-kiurites
     function kiurit(){
         nevInput.value = "";
@@ -32,8 +57,9 @@
             return ember.id === id;
         });
         if(index !== -1){
-            emberek.splice(index,1);
-            kiir();
+           emberek.splice(index,1);
+           mentes();
+           kiir();
         };
     }
 
@@ -44,15 +70,14 @@
         });
         if(!kember){
             return;
-        };
+        }
         nevInput.value = kember.nev;
         korInput.value = kember.kor;
         poziInput.value = kember.pozi;
         berInput.value = kember.ber;
 
         szID = id;
-         kiir();
-    }
+    };
 
     //-Kiiratas
     function kiir(){
@@ -77,16 +102,16 @@
             const torles = document.createElement("button");
             torles.textContent = "Törlés";
 
-            torles.addEventListener("click",function(){
+            torles.addEventListener("click", function(){
                 torolE(ember.id);
             });
             td5.appendChild(torles);
 
-            //szerkesztes gomb
+            //szerkesztes
             const szerkesztes = document.createElement("button");
             szerkesztes.textContent = "Szerkesztés";
 
-            szerkesztes.addEventListener("click", function(){
+            szerkesztes.addEventListener("click",function(){
                 szerkesztE(ember.id);
             });
             td5.appendChild(szerkesztes);
@@ -101,67 +126,53 @@
         });
     }
 
+    //click
+    document.getElementById("kuldes").addEventListener("click",function(){
+        let nevVal = nevInput.value.trim();
+        let korVal = Number(korInput.value.trim());
+        let poziVal = poziInput.value.trim();
+        let berVal = Number(berInput.value.trim());
 
-
-
-//tomb letrehozasa
-const emberek = [];
-
-//szerkesztes eldontese
-let szID = null;
-
-//DOM cache
-const nevInput = document.getElementById("nev");
-const korInput = document.getElementById("kor");
-const poziInput = document.getElementById("pozicio");
-const berInput = document.getElementById("ber");
-
-//click esemeny
-document.getElementById("kuldes").addEventListener("click", function(){
-    let nevVal = nevInput.value.trim();
-    let korVal = Number(korInput.value.trim());
-    let poziVal = poziInput.value.trim();
-    let berVal = Number(berInput.value.trim());
-
-    if(!nevVal){
-        hibaE(nevInput,"Add meg a neved!");
-        return;
-    };
-    if(isNaN(korVal) || korVal < 18 || korVal > 70){
-        hibaE(korInput,"min:18 - max:70!");
-        return;
-    };
-    if(!poziVal){
-        hibaE(poziInput,"Add meg a poziciót!");
-        return;
-    };
-    if(isNaN(berVal) || berVal <= 0 || berVal > 10000000){
-        hibaE(berInput,"Helytelen összeget adtál meg!");
-        return;
-    };
-
-
-    if(szID === null){
-        const ujEmber = {
-            id:Date.now(),
-            nev:nevVal,
-            kor:korVal,
-            pozi:poziVal,
-            ber:berVal
+        if(!nevVal){
+            hibaE(nevInput,"Add meg a neved!");
+            return;
         };
-        emberek.push(ujEmber);
-    }
-    else{
-        const index = emberek.findIndex(function(ember){
-          return ember.id === szID;
-        });
-        if(index !== -1){
-            emberek[index].nev = nevVal;
-            emberek[index].kor = korVal;
-            emberek[index].pozi = poziVal;
-            emberek[index].ber = berVal;
+        if(isNaN(korVal) || korVal < 18 || korVal > 70){
+            hibaE(korInput,"min:18 - max:70!");
+            return;
         };
-    };
-    kiir();
-    kiurit();
-});
+        if(!poziVal){
+            hibaE(poziInput,"Add meg a pozid!");
+            return;
+        };
+        if(isNaN(berVal) || berVal <= 0 || berVal > 10000000){
+            hibaE(berInput,"Helytelen összeget adtal meg!");
+            return;
+        };
+
+        if(szID === null){
+            const ujEmber = {
+                id: Date.now(),
+                nev:nevVal,
+                kor:korVal,
+                pozi:poziVal,
+                ber:berVal
+            };
+            emberek.push(ujEmber);
+            mentes();
+        }
+        else{
+            const index = emberek.findIndex(function(ember){
+                return ember.id === szID;
+            });
+            if(index !== -1){
+                emberek[index].nev = nevVal;
+                emberek[index].kor = korVal;
+                emberek[index].pozi = poziVal;
+                emberek[index].ber = berVal;
+                mentes();
+            };
+        };
+          kiir();
+            kiurit();
+    });
